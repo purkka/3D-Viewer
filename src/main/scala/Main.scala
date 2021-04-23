2440d4b92b4006c120354225cf7cd3d21e9df9d8
Erika Marttinen <erika.marttinen@aalto.fi>
2021-04-23 16:37:46 +0300

    Update Main
    - Enable file chooser
    - Add customizable light colors
    - Add error popup
    - Add rotation toggle
    - Disable resizing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,52 +1,100 @@
 import scalafx.application.JFXApp
 import graphics._
 import graphics.mesh._
-import graphics.scene.{AmbientLight, Camera, DirectionalLight, MeshObject, PointLight}
-import scalafx.scene.control.Button
-import scalafx.scene.layout.VBox
+import graphics.scene._
+import javafx.geometry.Insets
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control.{Button, Label}
+import scalafx.scene.layout.{HBox, VBox}
 import scalafx.scene.paint.Color
 import scalafx.stage.FileChooser
 
+import java.io.File
 import scala.math.toRadians
 
 object Main extends JFXApp {
-    val width = 800
-    val height = 600
-
-    val obj = new MeshObject(ObjParser.loadMesh("resources/monke.obj"))
-    val obj2 = new MeshObject(ObjParser.loadMesh("resources/monke.obj"))
-    obj.position = Vec4(0 , 0, -3)
-    obj2.position = Vec4(0 , 2, -3)
+    var obj = new MeshObject(Cube)
 
     // click and drag to move camera direction
     val camera = new Camera(toRadians(70))
+    camera.position = Vec4(0, 0, 3)
     val mouseHandler = new MouseHandler(camera)
     val keyHandler = new KeyHandler(camera, 1)
 
     val light0 = new AmbientLight()
-    val light1 = new DirectionalLight(Vec4(-1, -1, 0), Color.Red)
-    val light2 = new DirectionalLight(Vec4(1, -1, 0), Color.Blue)
-    val light3 = new DirectionalLight(Vec4(0, -1, -1), Color.Green)
+    val light1 = new DirectionalLight(Vec4(-1, -1, -1), Color.rgb(220, 220, 220))
+    val light2 = new PointLight(Vec4(-5, 0, 0), Color.Blue)
 
-    val canvas = new Canvas(width, height, mouseHandler, keyHandler)
-    val sc = new graphics.scene.Scene(camera, Vector(obj, obj2), Vector(light0, light1, light2, light3), canvas)
+    val canvas = new Canvas(900, 600, mouseHandler, keyHandler)
+    val sc = new graphics.scene.Scene(camera, Vector(obj), Vector(light0, light1, light2), canvas)
 
     var angle = 0.0
     val rotationalSpeed = 0.8
+    var rotation = true
 
     val timer = new Timer(delta => {
-        angle += rotationalSpeed * delta
-        obj.rotation = Quaternion(Vec4(0, 1, 0), 0.9 * angle)
-        obj2.rotation = Quaternion(Vec4(0, 1, 0), -3 * angle)
+        if (rotation) {
+            angle += rotationalSpeed * delta
+            obj.rotation = Quaternion(Vec4(0, 1, 0), 0.9 * angle)
+        }
         camera.position = camera.position + keyHandler.update(delta)
         canvas.draw(sc.render)
     })
 
+
+    val fc = new FileChooser()
+    val initPath = new File(System.getProperty("user.dir"), "resources")
+    if (initPath.isDirectory) fc.setInitialDirectory(initPath)
+    val fileButton = new Button("Select OBJ file")
+    fileButton.setOnAction(_ => {
+        val selectedFile = fc.showOpenDialog(stage)
+        if (selectedFile != null) {
+            try {
+                obj = new MeshObject(ObjParser.loadMesh(selectedFile.getAbsolutePath))
+            } catch {
+                case oe: CorruptedObjFileException =>
+                    new ExceptionDialog(AlertType.Error, oe)
+            }
+            sc.objects = Vector(obj)
+            angle = 0.0
+        }
+    })
+
+    val rotationButton = new Button("Toggle rotation")
+    rotationButton.setOnAction(_ => rotation = !rotation)
+
+    val colorSlider1: ColorChooser = new ColorChooser(Color.LightGray) {
+        def update(c: Color): Unit = light1.color = c
+    }
+    val colorSlider2: ColorChooser = new ColorChooser(Color.Black) {
+        def update(c: Color): Unit = light2.color = c
+    }
+
+    val cs1Label = new Label("Adjust directional light")
+    cs1Label.setStyle("-fx-font-weight: bold")
+    val cs2Label = new Label("Adjust point light")
+    cs2Label.setStyle("-fx-font-weight: bold")
+
+    val vhbox = new VBox(fileButton, rotationButton)
+    vhbox.setPadding(new Insets(20, 10, 20, 10))
+    vhbox.setSpacing(10)
+    val vboxcs1 = new VBox(cs1Label, colorSlider1)
+    vboxcs1.setSpacing(5)
+    val vboxcs2 = new VBox(cs2Label, colorSlider2)
+    vboxcs2.setSpacing(5)
+    val hbox = new HBox(vhbox, vboxcs1, vboxcs2)
+    hbox.setPadding(new Insets(10, 10, 10, 10))
+    hbox.setSpacing(10)
+    val vbox = new VBox(canvas, hbox)
+    val scc = new scalafx.scene.Scene(vbox)
+
     // for the animation
     stage = new JFXApp.PrimaryStage {
-        title.value = "3D viewer"
-        scene = canvas
+        title.value = "3D Object Viewer"
+        scene = scc
     }
+
+    stage.setResizable(false)
 
     timer.start()
 }
--- src/main/scala/graphics/scene/Scene.scala
+++ src/main/scala/graphics/scene/Scene.scala
@@ -2,11 +2,9 @@ package graphics.scene
 
 import graphics._
 import scalafx.scene.Node
-import scalafx.scene.shape.Polygon
-
 import scala.collection.mutable
 
-class Scene(camera: Camera, objects: Vector[Object], lights: Vector[Light], canvas: Canvas) {
+class Scene(camera: Camera, var objects: Vector[Object], lights: Vector[Light], canvas: Canvas) {
     val result = new mutable.PriorityQueue[(N, Node)]()(Ordering.by(t => t._1))
 
     def render: RenderQueue = {
